6ff0bf3ca465560454b870e3bf782cf4f78d191d
app182019 <47757230+app182019@users.noreply.github.com>
2019-02-25 19:36:37 +0200

    Create exercise.js

---
+++ exercise.js
@@ -0,0 +1,59 @@
+//1
+function helloWorld()
+{
+  console.log("hello world!")
+}
+
+helloWorld()
+helloWorld()
+var x = 1
+helloWorld
+
+
+function a_is_bigger()
+{
+  console.log("a is bigger!")
+}
+
+function b_is_bigger()
+{
+  console.log("b is bigger!")
+}
+
+var a = parseInt(prompt("number a"))
+var b = parseInt(prompt("number b"))
+if (a > b)
+  {
+    a_is_bigger()
+  }
+else
+  {
+    b_is_bigger()
+  }
+
+
+function average(a1, b1, c1)
+{
+  var avg = (a1 + b1 + c1) /3
+  console.log(avg)
+}
+
+average (8, 5, 10)
+
+var a2 = parseInt(prompt("number a"))
+var b2 = parseInt(prompt("number b"))
+var c2 = parseInt(prompt("number c"))
+average(a2, b2, c2)
+
+
+function flip(x)
+{
+  return -1 * x
+}
+ var r8 =  flip(8)
+ console.log( flip(-8) )
+
+
+
+
+
