48f635e2d9f98f5a285cb0e915f855dbd2a2939c
app182019 <47757230+app182019@users.noreply.github.com>
2019-02-25 18:07:50 +0200

    Create program.js

---
+++ program.js
@@ -0,0 +1,32 @@
+function noparameters()
+{
+  
+  
+}
+
+function print(msg)
+{
+  console.log(msg)
+}
+
+function mul(a, b)
+{
+  var c = a * b
+  return c
+}
+
+print("Hello world!")
+print("goodbye!")
+print(3)
+print(3 + 4)
+print("Hello world!")
+print("Hello world!")
+
+var sum = add(5, 100)
+print(sum)
+
+add(8, 10000)
+
+var x = parseInt(prompt("enter number, 0"))
+var y = mul(8, 7.6)
+
